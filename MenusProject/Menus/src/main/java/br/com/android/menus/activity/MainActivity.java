c5d69ed2432972fba551e7e6c057756091501c4d
Robson Knabben <robson.knabben@gmail.com>
2013-09-09 04:12:15 -0300

    Alterada a tela de splash screen

--- MenusProject/Menus/src/main/java/br/com/android/menus/activity/MainActivity.java
+++ MenusProject/Menus/src/main/java/br/com/android/menus/activity/MainActivity.java
@@ -1,15 +1,12 @@
 package br.com.android.menus.activity;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuItem;
 
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.client.RestClientException;
@@ -24,51 +21,26 @@ import br.com.android.menus.model.Ramo;
 
 public class MainActivity extends BaseActivity {
 
-    TextView text;
+    TextView mProgressInformation;
+    ProgressBar mProgressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //opção de voltar no menu
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressBar.setIndeterminate(true);
 
-        this.text = (TextView) findViewById(R.id.Texto);
-        this.text.setText("Mudeio Texto");
+        mProgressInformation = (TextView) findViewById(R.id.progressInformation);
 
         new AppAsyncTask(this).execute();
-
-        //startActivity(new Intent(this, RamosActivity.class));
-        //finish();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getSupportMenuInflater().inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            //função da opção de voltar do menu
-            //case R.id.homeAsUp:
-            //    NavUtils.navigateUpFromSameTask(this);
-            //    return true;
-            case R.id.action_settings:
-                this.text.setText("aPERTEI SETTINGS");
-                startActivity(new Intent(this, AppFragmentActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK).putExtra(EXTRA_MENU_DRAWER_OPENED, true));
-                finish();
-                return true;
-            //mais cases para mais opções
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     private class AppAsyncTask extends AsyncTask<Void, String, Integer> {
         Context mContext;
-        ProgressDialog mProgressDialog;
+        //ProgressDialog mProgressDialog;
 
         public AppAsyncTask(Context context) {
             this.mContext = context;
@@ -76,10 +48,7 @@ public class MainActivity extends BaseActivity {
 
         @Override
         protected void onPreExecute() {
-            mProgressDialog = new ProgressDialog(mContext);
-            mProgressDialog.setIndeterminate(true);
-            mProgressDialog.setMessage("Aguarde...");
-            mProgressDialog.show();
+            super.onPreExecute();
         }
 
         @Override
@@ -122,19 +91,18 @@ public class MainActivity extends BaseActivity {
 
         @Override
         protected void onProgressUpdate(String... values) {
-            mProgressDialog.setMessage(values[0]);
+            mProgressInformation.setText(values[0]);
+
         }
 
         @Override
         protected void onPostExecute(Integer integer) {
-            if (integer == 0){
-                mProgressDialog.dismiss();
-                startActivity(new Intent(mContext, AppFragmentActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK).putExtra(EXTRA_MENU_DRAWER_OPENED, true));
-                finish();
+            if (integer == 1){
+                publishProgress("Não foi possível conectar com o servidor. Verifique sua conexão.");
             }
-            else{
 
-            }
+            startActivity(new Intent(mContext, AppFragmentActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK).putExtra(EXTRA_MENU_DRAWER_OPENED, true));
+            finish();
         }
     }
 }
--- MenusProject/Menus/src/main/java/br/com/android/menus/app/AppRestTemplate.java
+++ MenusProject/Menus/src/main/java/br/com/android/menus/app/AppRestTemplate.java
@@ -6,19 +6,16 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-/**
- * Created by Robson on 05/08/13.
- */
 public class AppRestTemplate extends RestTemplate {
     public AppRestTemplate() {
         if (getRequestFactory() instanceof SimpleClientHttpRequestFactory) {
             Log.d("HTTP", "HttpUrlConnection is used");
-            ((SimpleClientHttpRequestFactory) getRequestFactory()).setConnectTimeout(10 * 1000);
-            ((SimpleClientHttpRequestFactory) getRequestFactory()).setReadTimeout(10 * 1000);
+            ((SimpleClientHttpRequestFactory) getRequestFactory()).setConnectTimeout(5 * 1000);
+            ((SimpleClientHttpRequestFactory) getRequestFactory()).setReadTimeout(5 * 1000);
         } else if (getRequestFactory() instanceof HttpComponentsClientHttpRequestFactory) {
             Log.d("HTTP", "HttpClient is used");
-            ((HttpComponentsClientHttpRequestFactory) getRequestFactory()).setReadTimeout(10 * 1000);
-            ((HttpComponentsClientHttpRequestFactory) getRequestFactory()).setConnectTimeout(10 * 1000);
+            ((HttpComponentsClientHttpRequestFactory) getRequestFactory()).setReadTimeout(5 * 1000);
+            ((HttpComponentsClientHttpRequestFactory) getRequestFactory()).setConnectTimeout(5 * 1000);
         }
     }
 }
